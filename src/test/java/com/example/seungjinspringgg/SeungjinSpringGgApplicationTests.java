9c11f650591b846c914b79b05b15d573dbe8eb5c
ohksj77 <ohksj77@gmail.com>
2022-09-23 20:47:04 +0900

    spring init

---
+++ src/test/java/com/example/seungjinspringgg/SeungjinSpringGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seungjinspringgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungjinSpringGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
